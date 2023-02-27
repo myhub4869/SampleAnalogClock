015d66a967d10d34e07f1d26ca8422e3450cb8dc
kenmiya4869 <kenmiya.sns@gmail.com>
2023-02-27 13:39:27 +0900

    ServiceWorkerを追加（仮）

--- assets/js/registerServiceWorker.js
+++ assets/js/registerServiceWorker.js
@@ -2,15 +2,15 @@ const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
       const registration = await navigator.serviceWorker.register(
-        '/assets/js/serviceWorker.js',
+        '/serviceWorker.js',
         {
-          scope: '/assets/js/',
+          scope: '/ana_clock/',
         }
       );
       console.log(registration);
       if (registration.installing) {
         console.log('Service worker installing');
-        navigator.serviceWorker.register('/assets/js/serviceWorker.js').then(function(registration) {
+        navigator.serviceWorker.register('/serviceWorker.js').then(function(registration) {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
           // Service Workerの登録に失敗したときの処理をここに記述します。
